b1c67cccbcc32eb4f892b353aaf2ea5cdfa83796
Than Doan Dang Khoa <dangkhoa114@gmail.com>
2016-08-15 14:20:42 +0700

    fix href, src attributes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,9 +13,9 @@
 
     <!-- Styles -->
     {{-- link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> --}}
-    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/bootstrap.min.css}}">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css}}">
     <style>
         body {
             font-family: 'Lato';
